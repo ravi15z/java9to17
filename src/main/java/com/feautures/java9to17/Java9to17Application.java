c55c2b050689702f6f2e71e1f7773ad945f429ff
ravi15z <rv.gaikwad15@gmail.com>
2024-04-02 10:41:06 +0530

    1st commit

---
+++ src/main/java/com/feautures/java9to17/Java9to17Application.java
@@ -0,0 +1,13 @@
+package com.feautures.java9to17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java9to17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java9to17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/feautures/java9to17/Java9to17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feautures.java9to17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java9to17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
