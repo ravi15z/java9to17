3760cb28a9db1b135ef17fa9fef69eff30e9eb6e
ravi15z <rv.gaikwad15@gmail.com>
2024-04-02 11:08:21 +0530

    private methods added

--- src/main/java/com/feautures/java9to17/Java9to17Application.java
+++ src/main/java/com/feautures/java9to17/Java9to17Application.java
@@ -3,11 +3,17 @@ package com.feautures.java9to17;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.feautures.java9to17.privateMethods.SendNotification;
+import com.feautures.java9to17.privateMethods.SendNotificationImpl;
+
 @SpringBootApplication
 public class Java9to17Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java9to17Application.class, args);
+		SendNotification sendNotification = new SendNotificationImpl();
+		sendNotification.sendNotification();
+		sendNotification.sendNotifications();
 	}
 
 }
---
+++ src/main/java/com/feautures/java9to17/privateMethods/SendNotificationImpl.java
@@ -0,0 +1,5 @@
+package com.feautures.java9to17.privateMethods;
+
+public class SendNotificationImpl implements SendNotification {
+
+}
