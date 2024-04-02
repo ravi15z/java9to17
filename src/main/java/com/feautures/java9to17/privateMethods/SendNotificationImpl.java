5bd74c7e763e60cf71f28dd8be936b41c2a21661
ravi15z <rv.gaikwad15@gmail.com>
2024-04-02 11:37:19 +0530

    tryWithResourses

--- src/main/java/com/feautures/java9to17/privateMethods/SendNotificationImpl.java
+++ src/main/java/com/feautures/java9to17/privateMethods/SendNotificationImpl.java
@@ -1,5 +1,15 @@
 package com.feautures.java9to17.privateMethods;
 
+import org.springframework.boot.SpringApplication;
+
+import com.feautures.java9to17.Java9to17Application;
+
 public class SendNotificationImpl implements SendNotification {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Java9to17Application.class, args);
+        SendNotification sendNotification = new SendNotificationImpl();
+        sendNotification.sendNotification();
+        sendNotification.sendNotifications();
+    }
 }
---
+++ src/main/java/com/feautures/java9to17/tryWithResourses/Test.java
@@ -0,0 +1,14 @@
+package com.feautures.java9to17.tryWithResourses;
+
+public class Test implements AutoCloseable {
+
+    Test() {
+        System.out.println("Creating Resourses");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("closing the resourses");
+    }
+
+}
